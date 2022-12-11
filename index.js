ca21cab9ee45921f2f21312ea157d339a16f8d69
+ <+>
2022-12-12 00:27:13 +0300

    commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const baseUrl ="http://localhost:3000/films";
+const baseUrl = "http://localhost:3000/films";
 function fetchMovies(){
     fetch(baseUrl)
-         .then((response) => response.json())
-         .then(data) =>{
-            let li = document.createElement("li");
+        .then((response) => response.json())
+        .then((data) =>{
+            data.forEach((films) =>{
+                let li = document.createElement("li");
                 li.textContent = films.title;
                 li.addEventListener("click",
                 (e)=>{
@@ -16,16 +17,48 @@ function fetchMovies(){
                 films.title;
                 let img =
                 document.getElementById("movie-poster");
-                img.src =
-             films.poster;
-                let showTime =
+                   img.src =
+                films.poster;
+                   let showTime =
                 document.getElementById("showtime");
-                showTime.textContent =
-              films.showtime;
-              let runTime =
-              
+                  showTime.textContent =
+                films.showtime;
+                let runTime =
+                document.getElementById("runtime");
+                  runTime.textContent =
+                `${films.runtime} Minutes`;
+                let tickets =
+              document.querySelector("div#ticket-counter");
+                    tickets.textContent = films["capacity"] - films["tickets_sold"]
+                })
+                document.querySelector("ul#films").appendChild(li)
+            })
+        })
+    }fetchMovies()
 
-                  
+function baseMovie(){ fetch(baseUrl)
+    .then(response => response.json())
+    .then(data => {
+    document.querySelector("h3#movie-title").textContent = data[0]["title"]
+    document.querySelector("img#movie-poster").setAttribute("src",`${data[0]["poster"]}`)
+    document.querySelector("div#showtime").textContent = data[0]["showtime"]
+    document.querySelector("div#runtime").textContent = `${data[0]["runtime"]} Minutes`
+    document.querySelector("ul#films").firstElementChild.remove()
+    document.querySelector("div#ticket-counter").textContent = data[0]["capacity"] - data[0]["tickets_sold"]
+})
+}
+baseMovie()
 
-         }
-        }
+function buyTicket(){
+    let button = document.querySelector("button#buy-ticket")
+    button.addEventListener("click",function(){
+    let currentLi = document.querySelector("div#ticket-counter")
+    let number = parseInt(currentLi.textContent)
+    if(number >=1){
+        currentLi.textContent = currentLi.textContent -1}
+        else {document.querySelector("button#buy-ticket").textContent = "Sold Out"
+          alert("Sorry, No more tickets available!!")}
+      }
+      )
+      }
+      buyTicket()
