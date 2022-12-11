25d7852869169b2f94c0761a37dc2f80ee8718b5
+ <+>
2022-12-12 00:17:46 +0300

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const baseUrl ="http://localhost:3000/films";
+function fetchMovies(){
+    fetch(baseUrl)
+         .then((response) => response.json())
+         .then(data) =>{
+            
+         }
+        }
